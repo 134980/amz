1c6b0f3d8513de78328508d3b265371c32fecb53
AlvinGhazali <71711909+AlvinGhazali@users.noreply.github.com>
2020-10-05 22:52:52 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+pkg install figlet
+clear
+figlet install | lolcat
+sleep 2
+clear
+pkg install nano
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pkg install libcurl
+clear
+gem install lolcat
+clear
+pip2 install requests
+clear
+pip2 install mechanize
+clear
+pip2 install termocolor
+echo "selesai"
+clear
+python2 amz.py
